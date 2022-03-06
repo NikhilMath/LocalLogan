f6810aa26904a9f2ec0f9f9392637fd17de2a3e4
Nikhil Math <erulemath@gmail.com>
2022-03-06 08:04:31 -0700

    initial push

---
+++ locallogan/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <Home></Home>
+  </div>
+</template>
+
+<script>
+import Home from './components/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Home
+  }
+}
+</script>
+
+<style>
+*,*::before,*::after {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+}
+</style>
---
+++ locallogan/src/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+	<h1>Hello world</h1>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
