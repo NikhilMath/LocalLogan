0bbf17183be22914d8fc3648e135bc92a0fdf577
Nikhil Math <erulemath@gmail.com>
2022-03-06 08:27:59 -0700

    tried to do something

--- locallogan/src/components/Home.vue
+++ locallogan/src/components/Home.vue
@@ -1,5 +1,13 @@
 <template>
 	<h1>Hello world</h1>
+  <div id="app">
+     <div class="alert alert-primary" role="alert">
+        A simple primary alert—check it out!
+      </div>
+      <div class="alert alert-secondary" role="alert">
+        A simple secondary alert—check it out!
+      </div>
+  </div>
 </template>
 
 <script>
