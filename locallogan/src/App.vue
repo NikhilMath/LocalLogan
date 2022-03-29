27d79a71d2b0a524bd74dfafbb6cf1fa5e14c243
Austin Thornley <austinjt1012@gmail.com>
2022-03-28 20:05:28 -0600

    Work on landing page

--- locallogan/src/App.vue
+++ locallogan/src/App.vue
@@ -1,30 +1,23 @@
 <template>
-  <div class="m-8" id="app">
-		<img src="./assets/black.png" style="margin: 0 auto;" alt="LocalLogan Logo" width="150">
-		<h1 class="text-center">Hey there! We're under construction - but will be back soon with some great new features to improve the qualify of life for the citizens of Logan and the greater Cache Valley area.</h1>
-		<h5 class="text-center mt-4">Questions? <a href="mailto:erulemath@gmail.com">Email us</a>!</h5>
-		<!-- <Navbar></Navbar> -->
-		<!-- <router-view></router-view> -->
+  <div id="app">
+		<Navbar></Navbar>
+		<section class="hero">
+			<h1>Local</h1>
+			<h1 class="ml-20">Logan</h1>
+		</section>
   </div>
 </template>
 
 <script>
-// import Navbar from './components/Navbar.vue'
+import Navbar from './components/Navbar.vue';
 
 export default {
   name: 'App',
-  components: {
-    // Home,
-		// Navbar,
-		// Activities,
-		// Creators,
-		// DateIdeas,
-		// Restaurants
-  },
+	components: {
+		Navbar
+	},
 	data() {
-		return {
-			msg: "I'm centered"
-		}
+		return {}
 	},
 	mounted() {
 		document.title = 'LocalLogan - Home';
@@ -33,13 +26,6 @@ export default {
 </script>
 
 <style>
-/* 
-	TODO: 
-	1. Mess with some quick designs in Figma
-	2. Create components to match previous pages 
-
-*/
-
 *, *::before, *::after {
 	margin: 0;
 	padding: 0;
@@ -51,5 +37,27 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+	width: 100vw;
+	height: 100vh;
+	background: url(./assets/Map.png) center center / cover no-repeat;
+}
+
+.hero {
+	background-color: #49723F;
+	color: black;
+	height: 400px;
+	max-width: 900px;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	position: relative;
+  top: 20vh;
+	border-radius: 3px;
+	margin-left: -5px;
+}
+
+.hero>h1 {
+	font-size: 6rem;
+	padding-left: 40px;
 }
 </style>
