327a1a5dc1843a352c5d220089dfade69d532dfb
Austin Thornley <austinjt1012@gmail.com>
2022-03-29 21:36:29 -0600

    Updated router

--- locallogan/src/router/index.js
+++ locallogan/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './../components/Home.vue'
 import Restaurants from './../components/Restaurants.vue'
+// updated import
 
 Vue.use(Router)
 
