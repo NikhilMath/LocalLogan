f27ed7b3683cdc851e054e681356714cfec70107
Forrest Olson <fs.olson5@gmail.com>
2022-04-28 00:07:46 -0600

    restructuring: 1. assets folder 2. footer and header files 3. pages folder 4. components folder

--- index.js
+++ index.js
@@ -34,3 +34,42 @@ function darkModeToggle() {
   // localStorage.removeItem("theme");
   // localStorage.setItem("theme", "dark");
 }
+
+function initialize() {
+  fetch("./components/header.html")
+    .then((response) => response.text())
+    .then(
+      (text) => (document.getElementsByTagName("body")[0].innerHTML += text)
+    );
+  fetch("./pages/main.html")
+    .then((response) => response.text())
+    .then(
+      (text) => (document.getElementsByTagName("body")[0].innerHTML += text)
+    );
+  fetch("./components/footer.html")
+    .then((response) => response.text())
+    .then(
+      (text) => (document.getElementsByTagName("body")[0].innerHTML += text)
+    );
+
+  // let oldHeader = document.getElementById("footer");
+  // let newHeader = document.createElement("footer");
+  // newHeader.classList =
+  //   "h-10 flex justify-between items-center bg-navy-ish p-3";
+  // newHeader.innerHTML = `<div class="flex-1"></div>
+  //     <div class="">© by LocalLogan.com</div>
+  //     <a class="flex-1 text-right" href="./creators.html">Creators</a>`;
+  // oldHeader.parentNode.replaceChild(newHeader, oldHeader);
+
+  // is this better to all be in a separate file? or just the inner contents?
+
+  // document.getElementById("footer").parentNode.innerHTML += `
+  // <footer
+  //     id="footer"
+  //     class="h-10 flex justify-between items-center bg-navy-ish p-3"
+  //   >
+  //     <div class="flex-1"></div>
+  //     <div class="">© by LocalLogan.com</div>
+  //     <a class="flex-1 text-right" href="./creators.html">Creators</a>
+  //   </footer>`;
+}
